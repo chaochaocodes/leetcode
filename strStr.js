c412496d8ffeb99e6ba5652ae6a9e24da7b1347a
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2020-09-13 17:50:43 -0400

    strings, needle in haystack

---
+++ strStr.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+// Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+// For the purpose of this problem, return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+
+// Using match()
+var strStr = function(haystack, needle) {
+  if (needle.length === 0) return 0;
+  let findNeedle = haystack.match(needle)
+  return findNeedle === null ? -1 : findNeedle.index
+  // match returns null if not found
+  // match returns [ 'll', index: 2, input: 'hello', groups: undefined ]
+};
+
+// Using indexOf which returns -1 when not found
+let strStr = (haystack, needle) => haystack.indexOf(needle);
+
+
+haystack = "hello", needle = "ll" // 2
+haystack = "aaaaa", needle = "bba" // -1
+strStr(haystack, needle)
