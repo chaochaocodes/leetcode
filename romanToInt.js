4adecca88d407003a4e04d68b4a4aee60435d3f9
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2020-10-29 14:08:12 -0400

    solution with Map()

--- romanToInt.js
+++ romanToInt.js
@@ -14,6 +14,7 @@
 // input4 = "MCMXCIV"  // 1994, M=1000, CM= 900, XC= 90, IV= 4
 // input5 = "IV" //4
 
+// 1) Using switch, for loop, reduce
 var romanToInt = function(s) {
   const number = []
   for (i=0; i<s.length; i++) {
@@ -41,29 +42,48 @@ var romanToInt = function(s) {
         break;
     }
   }
-  console.log('convert:', number)
 
   // I can be placed before V (5) and X (10) to make 4 and 9. 
   // X can be placed before L (50) and C (100) to make 40 and 90. 
   // C can be placed before D (500) and M (1000) to make 400 and 900.
-  const finalSum = []
+  const result = []
   for (i=0; i < number.length; i++) {
-    if (number[i]== 100 && (number[i+1]==500 || number[i+1]==1000 )) {
-      finalSum.push(number[i+1] - number[i])
-      ++i;
-    } else if (number[i]== 10 && (number[i+1]==50 || number[i+1]==100 )) {
-      finalSum.push(number[i+1] - number[i])
-      ++i;
-    } else if (number[i]== 1 && (number[i+1]==5 || number[i+1]==10 )) {
-      finalSum.push(number[i+1] - number[i])
+    if (number[i]< number[i+1]) {
+      result.push(number[i+1] - number[i])
       ++i;
     } else {
-      finalSum.push(number[i])
+      result.push(number[i])
     }
   } 
-  console.log(finalSum)
-  return finalSum.reduce(function(a,b) { return a+b}, 0)
+  console.log(result)
+  return result.reduce(function(a,b) { return a+b }, 0)
 
 };
 
-romanToInt(input5)
+
+// 2) Using Map(), while loop
+// const romanToInt = s => {
+//   if (!s || s.length === 0) {
+//     return 0;
+//   }
+
+//   const map = new Map([['I', 1], ['V', 5], ['X', 10], ['L', 50], ['C', 100], ['D', 500], ['M', 1000]]);
+
+//   let i = s.length - 1;
+//   let result = map.get(s[i]);
+
+//   while (i > 0) {
+//     const curr = map.get(s[i]);
+//     const prev = map.get(s[i - 1]); 
+
+//     if (prev >= curr) {
+//       result += prev;
+//     } else {
+//       result -= prev;
+//     }
+
+//     i--;
+//   }
+
+//   return result;
+// };
