e2fbee702852ddce7c9b0f7bc99c42731ec13441
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2021-04-01 22:20:58 -0400

    add solution

--- top-interview-q/containsDuplicate.js
+++ top-interview-q/containsDuplicate.js
@@ -1,16 +1,36 @@
 /**
  * @param {number[]} nums
- * @param {number} k
  * @return {boolean}
  */
-var containsNearbyDuplicate = function(nums, k) {
-  for (let i = 0; i < nums.length; i++) {
-      for (let j = 0; j < i; j++) {
-          if (nums[i] == nums[j]) {
-              if (Math.abs(i - j) <= k) return true
-          }
-      }
-  }
-  return false
+
+// 1. Solution with sort and For loop
+ var containsDuplicate = function(nums) {
+    // edge case
+    if (nums === null || nums === []) return false
+    // clone array to avoid modifying input
+    let sorted = [...nums].sort()
+    // any duplicate int will be consecutive after sorting
+    for (i = 0; i < sorted.length; i++) {
+        if (sorted[i] == sorted[i + 1]) return true;             
+    }
+    return false;
 };
 
+
+// 2. Solution with Set()
+var containsDuplicate = function(nums) {
+    return nums.length !== [...new Set(nums)].length
+};
+
+// 3. Solution with hash table
+var containsDuplicate = function(nums) {
+    const hash = {}
+    for (let i = 0; i < nums.length; i++) {
+        if (!hash[nums[i]]) {
+            hash[nums[i]] = 1
+        } else {
+            return true
+        }
+    }
+    return false
+};
