924253b551aaef2462a0c3aeda6758e36beae7bc
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2021-04-08 13:55:58 -0400

    update comments

--- top-interview-q/intersection2Arrays.py
+++ top-interview-q/intersection2Arrays.py
@@ -3,12 +3,12 @@ nums1 = [4,9,5]
 nums2 = [9,4,9,8,4]
 
 import collections
-# 1. Using map, collections.Counter, list
+# 1. Map arrays to Counter, return list of intersecting elements
 def intersect(self, nums1, nums2):
     a, b = map(collections.Counter, (nums1, nums2))
     return list((a & b).elements())
 
-# 2. Variations
+# 2. Initiate Counter, convert array to Counter instances, get intersection
 def intersect(nums1, nums2):
     C = collections.Counter
     return list((C(nums1) & C(nums2)).elements())
@@ -19,20 +19,14 @@ def intersect(nums1, nums2):
     # # Counter({4: 1, 9: 1, 5: 1})
     # print(C(nums2))
     # # Counter({9: 2, 4: 2, 8: 1})
-
-        # print(C(nums1) & C(nums2))
-        # # Counter({4: 1, 9: 1})
-        # print((C(nums1) & C(nums2)).elements())
-        # # <itertools.chain object at 0x1077f0fd0>
+    # print(C(nums1) & C(nums2))
+    # # Counter({4: 1, 9: 1})
+    # print((C(nums1) & C(nums2)).elements())
+    # # <itertools.chain object at 0x1077f0fd0>
     
     # print(list(C(nums1) & C(nums2)))
     # # [4, 9]
     # print(list((C(nums1) & C(nums2)).elements()))
     # # [4, 9]
 
-
 intersect(nums1, nums2)
-
-# 3. Variation
-# def intersect(self, nums1, nums2):
-#     return list((collections.Counter(nums1) & collections.Counter(nums2)).elements())
