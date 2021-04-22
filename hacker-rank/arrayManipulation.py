d415a4bc772e4a24379b5ee741f337f3dd8aac09
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2021-04-21 23:57:33 -0400

    add dp solution

--- hacker-rank/arrayManipulation.py
+++ hacker-rank/arrayManipulation.py
@@ -12,26 +12,22 @@ query = [[1, 2, 100], [2, 5, 100], [3, 4, 100]]
 # 100 200 200 200 100
 
 def arrayManipulation(n, queries):
-  matrix = [[0 for x in range(n)] for y in range(3)]
-  print(queries)
-
-  test = []
-  for y in range(3):
-    row = []
-  # loop through queries to set range(start/stop)
-    start = queries[y][0]-1
-    stop = queries[y][1]
-    k = queries[y][2]
-    print('start: {}, stop:{}, k:{}'.format(start, stop, k))
+    # dynamic programming!!  
+    # loop queries, add and save k values to an array, return max
+    f = [0]*n
+    q = len(queries)
+    # for each query, from index 0 to 1, add index 2
+    for i in queries:
+        start = i[0]-1
+        print('i: {}'.format(i))
+        while start < i[1]:
+            # print('start: {}, end: {}'.format(start, i[1]))
+            f[start] += i[2]
+            start += 1
     
-    for x in range(n):
-      if x >= start and x < stop:
-        row.append(k)
-      else:
-        row.append(0)
-    test.append(row)
+    # print(f)
+    return max(f)
 
-    for i in test:
-      print(i)
-
-print(arrayManipulation(n, query))
+n = 5
+queries =[[1, 2, 100], [2, 5, 100], [3, 4, 100]]
+arrayManipulation(n, queries)
