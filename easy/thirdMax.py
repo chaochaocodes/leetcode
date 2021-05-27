fb27fe228ecdf1eba61b82721bd476b68268e72f
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2021-05-26 22:54:12 -0700

    add solution

---
+++ easy/thirdMax.py
@@ -0,0 +1,6 @@
+'''
+Given integer array nums, return the third maximum **distinct** number in this array. If the third maximum does not exist, return the maximum number.
+
+'''
+nums = [2,2,3,1] # Output: 1
+
