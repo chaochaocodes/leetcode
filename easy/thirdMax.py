4bbecddb62ad63c57939a27452da3686f0a002e3
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2021-05-26 22:55:20 -0700

    add solution

--- easy/thirdMax.py
+++ easy/thirdMax.py
@@ -4,3 +4,18 @@ Given integer array nums, return the third maximum **distinct** number in this a
 '''
 nums = [2,2,3,1] # Output: 1
 
+# Time complexity:  O(n)
+# Space complexity: O(n1)
+def thirdMax(nums):
+    unique = sorted(set(nums))
+    length = len(unique)
+    if length >= 3:
+        return unique[length-3]
+    else:
+        return max(unique)
+
+# Written as 3 lines
+def thirdMax(nums):
+    if len(set(nums)) < 3:
+        return max(nums)
+    return sorted(list(set(nums)))[-3]
