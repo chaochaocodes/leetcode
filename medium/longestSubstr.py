addfd635fed29f5b05113f07c733267c8478c600
Alicia Chao <57464564+chaochaocodes@users.noreply.github.com>
2021-06-03 20:28:05 -1100

    update solution

--- medium/longestSubstr.py
+++ medium/longestSubstr.py
@@ -8,12 +8,11 @@ s1 = "abcabcbb" # abc, 3
 s2 = "bbbbb" # b, 1
 s3 = "pwwkew" # wke, 3 (not pwke, not looking for a subsequence)
 
+# Approach 1: Sliding Window, O(n) time, O(1) space
 def lengthOfLongestSubstring(s):
-# sliding window!  O(n) time, O(1) space
-# use set() to check for repeating characters
-# hashmap for time/space efficiency,  {set(substr): len(substr)}
     n = len(s)
     l, r, res = 0, 0, 0
+    # use set() to check for repeating characters
     char = set() 
 
     while r < n:
@@ -27,22 +26,46 @@ def lengthOfLongestSubstring(s):
     
     print(res)
 
-# Faster approach
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        if not s: return 0
 
-        d=dict()
-        m=0
-        j=0
-        for i in range(len(s)):
-            if s[i] in d and j<=d[s[i]]:
-                j=d[s[i]]+1
-            else:
-                m = max(m,i-j+1)
+# Approach #2: Faster, using hashmap + pointer
+def lengthOfLongestSubstring(s):
+    if not s: return 0
 
-            # dict keeps track of current letter's index
-            d[s[i]]=i
-        return m
+    d=dict()
+    m=0
+    j=0
+    for i in range(len(s)):
+        if s[i] in d and j<=d[s[i]]:
+            j=d[s[i]]+1
+        else:
+            m = max(m,i-j+1)
+
+        # dict keeps track of current letter's index
+        d[s[i]]=i
+    return m
+
+
+# Approach 3: Counter() 
+# Hash for time/space efficiency,  {char: char_count}
+# Worst case is O(2N) = O(N) when all duplicate
+from collections import Counter
+def lengthOfLongestSubstring(s):
+    
+    chars = Counter()
+    prev, res = 0, 0
+
+    for pos, char in enumerate(s):    
+        chars[char] += 1
+        while chars[char] > 1:
+            chars[s[prev]] -= 1
+            prev += 1
+        
+        res = max(res, pos-prev+1)
+    
+    return res
+            
+
 
 lengthOfLongestSubstring(s0)
+
+
